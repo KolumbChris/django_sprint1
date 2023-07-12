7b8627898f055ee90a4f3235684e80e32bf848ee
Кристиан Никулин <chriskolumb@rambler.ru>
2023-07-12 15:52:26 +0300

    flake8

--- blogicum/blog/apps.py
+++ blogicum/blog/apps.py
@@ -3,8 +3,4 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-<<<<<<< HEAD
     name = 'blog'
-=======
-    name = 'blog'
->>>>>>> c8bf4cf37a2201c00b7e8339cdca8bab8518f7d9
